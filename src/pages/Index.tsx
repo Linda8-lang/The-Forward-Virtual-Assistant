f65264b8cffb27f885550e4c138dcb4e0d2b6762
Linda A. <68639945+Linda8-lang@users.noreply.github.com>
2025-09-12 10:42:16 -0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,7 +11,7 @@ const Index = () => {
       <main>
         <HeroSection />
         {/*Comment out the About, Services, Contact*/}
-        {/*<AboutSection />*/}
+        <AboutSection />
         {/*<ServicesSection />*/}
         {/*<ContactSection />*/}
       </main>
