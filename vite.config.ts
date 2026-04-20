f636bcbbc975694aed3d1146519aca112abdacb1
Linda8-lang <68639945+Linda8-lang@users.noreply.github.com>
2026-04-19 18:57:50 -0700

    Update for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/The-Forward-Virtual-Assistant/",
 
   server: {
     host: "::",
