ef91b5a8b2d42f22c6da2ebe7ac9db291970a90c
Linda8-lang <68639945+Linda8-lang@users.noreply.github.com>
2026-05-15 08:46:17 -0700

    Import the DRLS Project

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import DataAnalysisDashboard from "./pages/projects/DataAnalysisDashboard";
+import DynamicRowLevelSecurity from "./pages/projects/DynamicRowLevelSecurity";
 import VirtualSupportAutomation from "./pages/projects/VirtualSupportAutomation";
 import FinancialForecasting from "./pages/projects/FinancialForecasting";
 
